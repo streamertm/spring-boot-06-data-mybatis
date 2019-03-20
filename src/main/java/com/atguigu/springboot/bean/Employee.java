6c96ac66925b4843ad0f160f6eb9b34e0a9651bc
streamer <1320947155@qq.com>
2019-03-20 11:41:51 +0800

    my two springboot

---
+++ src/main/java/com/atguigu/springboot/bean/Employee.java
@@ -0,0 +1,60 @@
+package com.atguigu.springboot.bean;
+
+public class Employee {
+    private Integer id;
+    private String lastname;
+    private Integer gender;
+    private String email;
+    private Integer dId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getdId() {
+        return dId;
+    }
+
+    public void setdId(Integer dId) {
+        this.dId = dId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastname='" + lastname + '\'' +
+                ", gender=" + gender +
+                ", email='" + email + '\'' +
+                ", dId=" + dId +
+                '}';
+    }
+}
